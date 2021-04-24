b918fc45c0a577aeca3084dd23bb2692e6d6cd65
Ryan Helm <rjhelm@github.io>
2021-04-23 22:59:03 -0600

    find one product by tag code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -26,6 +26,28 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag,
+        as: 'products',
+      },
+    ],
+  }).then((dbTagData) => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'There was no tag found matching that id.' });
+      return;
+    }
+    res.json(dbTagData);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
